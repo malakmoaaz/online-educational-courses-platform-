4f48acd28a44ed31037103701797b935c822c924
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2024-11-08 19:35:33 -0500

    hero section purchase button course id added

--- client/src/Components/HeroSection.jsx
+++ client/src/Components/HeroSection.jsx
@@ -19,7 +19,7 @@ function HeroSection() {
         />
         <div className="flex justify-center">
           {isAuthenticated ? (
-            <PurchaseButton />
+            <PurchaseButton courseId={"it-course"} />
           ) : (
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Start Learning
